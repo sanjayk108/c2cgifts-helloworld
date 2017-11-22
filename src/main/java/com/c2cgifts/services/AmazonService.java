2fd48d80da8d416fb38bc09a46672ad85098745c
Sanjay Kataria <kataria.sanjay@gmail.com>
2017-11-22 09:40:05 -0500

    initial version

---
+++ src/main/java/com/c2cgifts/services/AmazonService.java
@@ -0,0 +1,24 @@
+package com.c2cgifts.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+public class AmazonService {
+
+	@RequestMapping("/")
+	String home () {
+		return "Hello earth";
+		
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonService.class, args);
+	}
+
+}
