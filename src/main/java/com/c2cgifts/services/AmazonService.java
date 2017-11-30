8427eca545acadd17590612b46a6e942e0d6dec0
Sanjay Kataria <kataria.sanjay@gmail.com>
2017-11-30 17:29:47 -0500

    change to test pipeline

--- src/main/java/com/c2cgifts/services/AmazonService.java
+++ src/main/java/com/c2cgifts/services/AmazonService.java
@@ -13,7 +13,7 @@ public class AmazonService {
 
 	@RequestMapping("/")
 	String home () {
-		return "Hello world - automated on kubernetes !";
+		return "Hello world - automated all pods on kubernetes !";
 		
 	}
 	
